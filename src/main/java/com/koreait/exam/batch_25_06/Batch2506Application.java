e0e76705b0c8fb19b5427768beb9d6185ed006af
ryuj7338 <ryuj7338@gmail.com>
2025-06-24 14:06:31 +0900

    jap사용 -> 회원 생성

--- src/main/java/com/koreait/exam/batch_25_06/Batch2506Application.java
+++ src/main/java/com/koreait/exam/batch_25_06/Batch2506Application.java
@@ -3,10 +3,12 @@ package com.koreait.exam.batch_25_06;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableBatchProcessing  // 어노테이션을 설정하면 알아서 자동으로 테이블 생성해줌
-public class Batch2506Application {
+@EnableBatchProcessing
+@EnableJpaAuditing
+public class Batch2506Application  {
 
     public static void main(String[] args) {
         SpringApplication.run(Batch2506Application.class, args);
--- src/main/java/com/koreait/exam/batch_25_06/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/koreait/exam/batch_25_06/job/helloWorld/HelloWorldJobConfig.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -14,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class HelloWorldConfig {
+public class HelloWorldJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
@@ -23,20 +25,49 @@ public class HelloWorldConfig {
     @Bean
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
-                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 부여 -> 파라미터
+//                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 부여 -> 파라미터
                 .start(helloWorldStep1())
+                .next(helloWorldStep2())
                 .build();
     }
 
     @Bean
+    @JobScope
     public Step helloWorldStep1() {
-        return stepBuilderFactory.get("helloWorldStep1").tasklet(helloWorldTasklet()).build();
+        return stepBuilderFactory
+                .get("helloWorldStep1")
+                .tasklet(helloWorldTasklet1())
+                .build();
     }
 
     @Bean
-    public Tasklet helloWorldTasklet() {
+    @StepScope
+    public Tasklet helloWorldTasklet1() {
         return (contribution, chunkContext) -> {
-            System.out.println("헬로월드!!!");
+            System.out.println("헬로월드!!!1111");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    @JobScope
+    public Step helloWorldStep2() {
+        return stepBuilderFactory
+                .get("helloWorldStep2")
+                .tasklet(helloWorldTasklet2())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet helloWorldTasklet2() {
+        return (contribution, chunkContext) -> {
+            System.out.println("헬로월드!!!2222");
+
+//            if(true){
+//                throw new Exception("실패 : 헬로 월드 태스클릿 2 실패");
+//            }
+
             return RepeatStatus.FINISHED;
         };
     }
