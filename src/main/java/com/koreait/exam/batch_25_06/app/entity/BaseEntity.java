1cc4b8a0b275b9fdd2eed5851716a242973f8146
ryuj7338 <ryuj7338@gmail.com>
2025-06-25 15:25:17 +0900

    주문 데이터 생성

--- src/main/java/com/koreait/exam/batch_25_06/app/entity/BaseEntity.java
+++ src/main/java/com/koreait/exam/batch_25_06/app/entity/BaseEntity.java
@@ -36,10 +36,10 @@ public class BaseEntity {
     @LastModifiedDate
     private LocalDateTime updateDate;
 
-    @Transient // 아래의 필드가 DB 컬럼이 되는 것을 막아줌
+    @Transient // 아래의 필드가 DB 컬림이 되는것을 막아
     @Builder.Default
     private Map<String,Object> extra = new LinkedHashMap<>();
-    
+
     public BaseEntity(Long id) {
         this.id = id;
     }
