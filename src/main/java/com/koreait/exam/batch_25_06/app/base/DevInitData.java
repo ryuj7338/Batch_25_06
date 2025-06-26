9bb545044c402d246bd275b93e372106c897f34d
ryuj7338 <ryuj7338@gmail.com>
2025-06-26 15:09:01 +0900

    2번 주문정보 ì생성 / 환불기록

--- src/main/java/com/koreait/exam/batch_25_06/app/base/DevInitData.java
+++ src/main/java/com/koreait/exam/batch_25_06/app/base/DevInitData.java
@@ -2,11 +2,13 @@ package com.koreait.exam.batch_25_06.app.base;
 
 import com.koreait.exam.batch_25_06.app.cart.service.CartService;
 import com.koreait.exam.batch_25_06.app.member.entity.Member;
+import com.koreait.exam.batch_25_06.app.order.entity.Order;
 import com.koreait.exam.batch_25_06.app.order.service.OrderService;
 import com.koreait.exam.batch_25_06.app.product.entity.Product;
 import com.koreait.exam.batch_25_06.app.product.entity.ProductOption;
 import com.koreait.exam.batch_25_06.app.member.service.MemberService;
 import com.koreait.exam.batch_25_06.app.product.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,7 @@ import java.util.Arrays;
 
 @Configuration
 @Profile("dev")
+@Slf4j
 public class DevInitData {
 
     @Bean
@@ -28,15 +31,19 @@ public class DevInitData {
             Member member3 = memberService.join("user3", password, "user3@test.com");
             Member member4 = memberService.join("user4", password, "user4@test.com");
 
-
+            // 1만원 충전
             memberService.addCash(member1, 10_000, "충전_무통장입금");
+            // 2만원 충전
             memberService.addCash(member1, 20_000, "충전_무통장입금");
-            memberService.addCash(member1, -5_000, "충전_일반");
+            // 5천원 사용
+            memberService.addCash(member1, -5_000, "출금_일반");
+
+            // 30만원 충전
+            memberService.addCash(member1, 300_000, "충전_무통장입금");
 
             long reshCash = memberService.getRestCash(member1);
 
-            System.out.println("member1 rest cash: " + reshCash);
-
+            log.info("member1 rest cash: " + reshCash);
 
             Product product1 = productService.create("반팔 1", 55000, 45000, "DDM-1",
                     Arrays.asList(
@@ -46,7 +53,7 @@ public class DevInitData {
                             new ProductOption("BLUE", "100")
                     )
             );
-            Product product2 = productService.create("셔츠 1", 66000,55000, "DDM-2",
+            Product product2 = productService.create("셔츠 1", 66000, 55000, "DDM-2",
                     Arrays.asList(
                             new ProductOption("WHITE", "95"),
                             new ProductOption("WHITE", "100"),
@@ -55,14 +62,46 @@ public class DevInitData {
                     )
             );
 
-            ProductOption productOption__RED_95 = product1.getProductOptions().get(0);
-            ProductOption productOption__BLUE_95 = product1.getProductOptions().get(2);
+            ProductOption productOption1__RED_95 = product1.getProductOptions().get(0);
+            ProductOption productOption1__BLUE_95 = product1.getProductOptions().get(2);
 
-            cartService.addItem(member1, productOption__RED_95, 1); // 회원1이 productOption__RED_95 1개 추가 / 총 수량 : 1
-            cartService.addItem(member1, productOption__RED_95, 2); // 회원1이 productOption__RED_95 2개 추가 / 총 수량 : 3
-            cartService.addItem(member1, productOption__BLUE_95, 1); // 회원1이 productOption__BLUE_95 1개 추가 / 총 수량 : 1
+            // 회원1 장바구니
+            cartService.addItem(member1, productOption1__RED_95, 1); // 회원1이 productOption__RED_95 1개 추가 / 총 수량 : 1
+            cartService.addItem(member1, productOption1__RED_95, 2); // 회원1이 productOption__RED_95 2개 추가 / 총 수량 : 3
+            cartService.addItem(member1, productOption1__BLUE_95, 1); // 회원1이 productOption__BLUE_95 1개 추가 / 총 수량 : 1
 
-            orderService.createFromCart(member1);
+            // 회원1의 주문1
+            Order order1 = orderService.createFromCart(member1);
+
+            // 회원2 장바구니
+
+            ProductOption productOption2__WHITE_95 = product2.getProductOptions().get(0);
+            ProductOption productOption2__BLACK_95 = product2.getProductOptions().get(2);
+
+            // 회원2 장바구니
+            cartService.addItem(member2, productOption2__WHITE_95, 1); // productOption2__WHITE_95 총 수량 1
+            cartService.addItem(member2, productOption2__WHITE_95, 2); // productOption2__WHITE_95 총 수량 3
+            cartService.addItem(member2, productOption2__BLACK_95, 1); // productOption2__BLACK_95 총 수량 1
+
+
+            // 회원2의 주문2
+            Order order2 = orderService.createFromCart(member2);
+
+            int order2PayPrice = order2.calculatePayPrice();
+            log.info("order2 pay price: " + order2PayPrice);
+
+            // 37만원 충전
+            memberService.addCash(member2,370_000,"충전_무통장입금");
+
+            log.info("member2 rest cash: " + member2.getRestCash());
+
+            // 결제 -> 예치금으로만결제
+            orderService.payByRestCashOnly(order2);
+
+            log.info("member2 rest cash: " + member2.getRestCash());
+
+            // 환불
+            orderService.refund(order2);
 
         };
     }
