4d44834ca0b1a1ddc681c0e74aded01c22524404
ryuj7338 <ryuj7338@gmail.com>
2025-06-26 13:44:30 +0900

    ãCash 개념 도입

--- src/main/java/com/koreait/exam/batch_25_06/app/product/entity/ProductOption.java
+++ src/main/java/com/koreait/exam/batch_25_06/app/product/entity/ProductOption.java
@@ -21,7 +21,19 @@ import static javax.persistence.FetchType.LAZY;
 public class ProductOption extends BaseEntity {
     private String color;
     private String size;
-    private Integer price;
+
+    private int price; // 권장 판매가 30000
+    private int salePrice; // 실제 판매가 40000
+    private int wholeSalePrice; // 도매가 < 30000
+    private int payPrice; // 결제 금액
+    private int refundPrice; // 환불 금액
+    private int pgFee; // 결제대행사 수수료
+    private int refundQuantity; // 환불 갯수
+    private boolean idPaid; // 결제 여부
+
+
+    private String displayColor;
+    private String displaySize;
 
     private boolean isSoldOut; // 관련 옵션의 판매가능 여부
     private int stockQuantity; // 보유 물건 수량
@@ -33,7 +45,9 @@ public class ProductOption extends BaseEntity {
 
     public ProductOption(String color, String size) {
         this.color = color;
+        this.displayColor = color;
         this.size = size;
+        this.displaySize = size;
     }
 
     public boolean isOrderable(int quantity) {
